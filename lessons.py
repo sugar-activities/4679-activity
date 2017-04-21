351d6ca691514a4b00db4654b1b2ed7a7bcdbfe4
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:09:37 -0400

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import glib
+
+LESSONS = (
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Origin Story')) + \
+        '</span>' + \
+        '\n\n',
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 1')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #1: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('There are rules to the Game of Life.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points: ')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • The Game of Life is made up of the same basic components as other games: players, goals, challenges and rules.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • To play the Game of Life well, we first need to identify the components and follow the rules.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • We are all playing the same Game of Life together, and therefore, the same rules apply to everyone.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Rules:  Guidelines for our behavior that keep us on the winning track of the Game of Life')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • What are some of the rules you are aware of in your life, and how are those rules helpful? Think about rules in your home, school, your neighborhood and different places you visit.')) + \
+        '\n' + \
+        glib.markup_escape_text(_("   • Describe the kind of world we would live in if there weren’t any rules. Why?")) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What rules are the hardest for you to follow? Why?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 2')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #2: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('Spiritual Fulfillment is what we really want.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Physical things are temporary and can be perceived with our five senses.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Spiritual fulfillment is long lasting and intangible.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Spiritual fulfillment is our main goal in life.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Our physical goals may change throughout our lives, while our desire for spiritual fulfillment remains constant.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Winning the Game of Life equals being happy and spiritually fulfilled.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Spiritual Fulfillment: All the feelings that we really want (happiness, love, excitement etc.) that cannot be measured, weighed, counted or perceived by our five senses')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Physical Things: Things that can be measured, weighed, counted or perceived by our five senses; things that do not last forever.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('Goals: A desired result or achievement.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Fill out a chart with the physical things you want and the spiritual fulfillment you receive from it.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Look at the chart and answer the following questions:')) + \
+        '\n' + \
+        glib.markup_escape_text(_('      ‣ Do you still want the same physical things today as you did a week ago, a month ago, a year ago or when you were a baby? Why?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('      ‣ Do you still want the same spiritual fulfillment today as you did a week ago, a month ago, a year ago or when you were a baby? Why?')),
+
+)
---
+++ videos.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+VIDEOS = (
+    ("0.ogv",
+     _("Start Here"),
+     _("Origin Story"),
+     ),
+    ("1.ogv",
+     _("Life is Like a Game"),
+     _("Rule #1: There are rules to the Game of Life\n\
+The Game of Life is made up of the same basic components as other games: players, goals, challenges and rules.\n\
+To play the Game of Life well, we first need to identify the components and follow the rules.\n\
+We are all playing the same Game of Life together, and therefore, the same rules apply to everyone."),
+     ),
+    ("2.ogv",
+     _("Fullfillment"),
+     _("Rule #2: Spiritual Fulfillment is what we really want\n\
+Physical things are temporary and can be perceived with our five senses.\n\
+Spiritual fulfillment is long lasting and intangible.\n\
+Spiritual fulfillment is our main goal in life.\n\
+Our physical goals may change throughout our lives, while our desire for spiritual fulfillment remains constant.\n\
+Winning the Game of Life equals being happy and spiritually fulfilled."),
+    ),
+)
